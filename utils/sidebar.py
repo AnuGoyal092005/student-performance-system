22900e371e95f589e8b99af4d395a8393b69c3be
Anu Goyal <anugoyalaggrawal@gmail.com>
2026-07-18 16:52:33 +0530

    Update sidebar.py

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 def show_sidebar():
-    with st.sidebar:
+    with st.sidebar: # with tells Python that everything inside this block should appear inside the sidebar.  It refers to the sidebar section of the Streamlit app.
         st.markdown("# 🎓")
         st.markdown("### Student Performance Prediction & Career Recommendation System")
-        st.markdown("---")
-        if st.button("🏠 Dashboard", use_container_width=True):
-            st.switch_page("Dashboard.py")
+        st.markdown("---") # It separates the title from the navigation buttons.
+        if st.button("🏠 Dashboard", use_container_width=True): # This line creates a full-width Dashboard button and checks if the user clicks it.
+            st.switch_page("Dashboard.py") # This line navigates the user to the Dashboard page.
         if st.button("👤 Student Profile", use_container_width=True):
             st.switch_page("pages/1_Student_Profile.py")
         if st.button("📊 Predict Performance", use_container_width=True):
