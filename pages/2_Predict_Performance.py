478d4eb7d579853693d0a44917577147cf653fba
Anu Goyal <anugoyalaggrawal@gmail.com>
2026-07-18 11:51:05 +0530

    Update comment for predicted GPA in performance prediction
    Clarified comment for predicted GPA calculation.

--- pages/2_Predict_Performance.py
+++ pages/2_Predict_Performance.py
@@ -53,7 +53,7 @@ student_data = {
 }
 
 input_df = pd.DataFrame([student_data])[model_columns] # [student_data]--> Makes a list containing one dictionary. pd.DataFrame(...)--> Converts it into a DataFrame. [model_columns]--> Reorders columns exactly like the training dataset. This prevents prediction errors.
-predicted_gpa = model.predict(input_df)[0]
+predicted_gpa = model.predict(input_df)[0] # [0] gets the first prediction.
 predicted_gpa = round(max(0, min(4, predicted_gpa)), 2)
 st.session_state['predicted_gpa'] = predicted_gpa
 predicted_percentage = round((predicted_gpa / 4) * 100, 1)
